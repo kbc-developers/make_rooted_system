2e594985e387cd004a5378d7bc1f949cbcb6f929
ma34s <ma34s.sss@gmail.com>
2013-05-30 23:42:25 +0900

    fix superuser relation  thanks @virifi

--- sed/type3/install-recovery.sh
+++ sed/type3/install-recovery.sh
@@ -361,7 +361,7 @@ busybox ln -sf busybox /system/xbin/zcip
 #su (for virifi 's method)
 busybox cp /system/etc/su_client /system/xbin/su
 chown root.root /system/xbin/su
-chmod 6755 /system/xbin/su
+chmod 755 /system/xbin/su
 
 
 chown root.root /system/xbin/superuser_su

886011307d65537d0f0ccb0fc6429abf217649fc
ma34s <ma34s.sss@gmail.com>
2014-02-11 09:34:25 +0900

    update Copyright

--- user_custom.sh
+++ user_custom.sh
@@ -1,4 +1,4 @@
-# Copyright 2011-2013 sakuramilk ma34
+# Copyright 2011-2014 sakuramilk ma34s
 #
 #   Licensed under the Apache License, Version 2.0 (the "License");
 #   you may not use this file except in compliance with the License.

e77d32b7ae2bdcc282e351acb61364d34b3b0b49
HomuHomu <ecoco666@gmail.com>
2014-05-20 22:54:18 +0900

    SC04F :Initial settings

---
+++ user/SC04F/Sample/install.sh
@@ -0,0 +1,29 @@
+# Copyright 2013 ken,ma34s
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#!/bin/bash
+# -------------------------------------------------------
+CURDIR=`dirname $0`
+FACTORYFS=$1
+
+echo ">>>>> Sample User Script..."
+# if you want someting 
+# you can write process here.
+# this script called afeter delete_pre-install and su_install.
+#
+# $FACTORYFS : /system dir
+#	exsample) $FACTORYFS/app -> /system/app
+#
+#echo "     Volume.db"
+#sudo cp -f $CURDIR/xxxxx $FACTORYFS/yyyyy/xxxxx
+
